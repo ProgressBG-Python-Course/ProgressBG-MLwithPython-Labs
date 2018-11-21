a579cb07a343ba38b941e6602e26e4e3fc01b7df
Iva E. Popova <progressbg.python.course@gmail.com>
2018-11-21 22:01:13 +0200

    Lab10: 21.11.2108 DONE: Naive Bayes NEXT: next TODO: Upload np.vectorise examples in Slides; check TODOs

--- naive_bayes_classifiers/nparray_mappings_discussion.py
+++ naive_bayes_classifiers/nparray_mappings_discussion.py
@@ -1,3 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 0.8.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.7.0
+# ---
+
 import numpy as np
 
 def target_colors(y):
@@ -21,13 +45,11 @@ def target_colors(y):
 
   # if we need more than binary mapping:
   c_map = {
-    '1': ADULT_COLOR,
-    '0': CHILD_COLOR
+    1: ADULT_COLOR,
+    0: CHILD_COLOR
   }
 
-  return np.vectorize(lambda i: c_map[str(i)])(y)
-
-
+  return np.vectorize(lambda i: c_map[i])(y)
 
 CHILD_COLOR = 'green'
 ADULT_COLOR = 'red'
